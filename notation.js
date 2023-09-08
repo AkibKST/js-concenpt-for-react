34d428df85306d99f7f867e9b764a0f1d572a510
AkibKST <akibkst22@gmail.com>
2023-09-08 14:13:30 +0600

    notation , dot & bracket

---
+++ notation.js
@@ -0,0 +1,15 @@
+const person ={
+    name : 'lal nil holud mia',
+    profession: 'Trafic surgeon',
+    age:39,
+    address:'shahabug'
+}
+
+// dot notation
+const prof1 = person.profession;
+
+// bracket notation
+const prof2 = person["profession"];
+ const pName = 'profession';
+ const prof3 = person[pName];
+ console.log(prof3);
